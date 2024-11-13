fb2ec73513da29513711b390f2274799ecf8248b
Oleksii Palamarchuk <144825066+PalamarchukOleksii@users.noreply.github.com>
2024-11-13 02:03:56 +0200

    implement 7 feauture

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -3,25 +3,26 @@ from itertools import product
 import os
 import shutil
 import sys
-import warnings
 import time
 
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-from sklearn.datasets._samples_generator import make_circles
+from sklearn.cluster import DBSCAN
 from sklearn.datasets._samples_generator import make_blobs
-from sklearn.exceptions import ConvergenceWarning
+from sklearn.datasets._samples_generator import make_circles
 from sklearn.inspection import DecisionBoundaryDisplay
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import (
+    accuracy_score,
+    adjusted_mutual_info_score,
+)
 from sklearn.metrics import silhouette_score, adjusted_rand_score
 from sklearn.model_selection import GridSearchCV
-from sklearn.cluster import DBSCAN
 
 DATASET_CIRCLES_ID = 1
 DATASET_BLOBS_ID = 2
 
-TRAIN_SPLIT_RATIO = 0.7
+TRAIN_SPLIT_RATIO = 1
 
 
 def get_dataset_name_by_id(dataset_id: int) -> str | None:
@@ -34,8 +35,8 @@ def get_dataset_name_by_id(dataset_id: int) -> str | None:
 
 def load_dataset(dataset_id: int) -> pd.DataFrame | None:
     if dataset_id == DATASET_CIRCLES_ID:
-        X, y = make_circles(10000, factor=.1, noise=.1)
-        dataset = pd.DataFrame(X, columns=["Feature_1", "Feature_2"])
+        x, y = make_circles(10000, factor=0.1, noise=0.1)
+        dataset = pd.DataFrame(x, columns=["Feature_1", "Feature_2"])
         dataset["Label"] = y.astype(int)
         return dataset
 
@@ -45,9 +46,14 @@ def load_dataset(dataset_id: int) -> pd.DataFrame | None:
         n_samples_3 = 300
         centers = [[0.0, 0.0], [2.5, 2.5], [-2.5, -2.5]]
         clusters_std = [1.5, 0.5, 1.0]
-        X, y = make_blobs(n_samples=[n_samples_1, n_samples_2, n_samples_3], centers=centers, cluster_std=clusters_std,
-                          random_state=0, shuffle=False)
-        dataset = pd.DataFrame(X, columns=["Feature_1", "Feature_2"])
+        x, y = make_blobs(
+            n_samples=[n_samples_1, n_samples_2, n_samples_3],
+            centers=centers,
+            cluster_std=clusters_std,
+            random_state=0,
+            shuffle=False,
+        )
+        dataset = pd.DataFrame(x, columns=["Feature_1", "Feature_2"])
         dataset["Label"] = y.astype(int)
         return dataset
     else:
@@ -58,11 +64,11 @@ def load_dataset(dataset_id: int) -> pd.DataFrame | None:
 
 
 def plot_dataset(
-        dataset_id: int,
-        df: pd.DataFrame,
-        save_plot=True,
-        save_path="plots",
-        show_plot=False,
+    dataset_id: int,
+    df: pd.DataFrame,
+    save_plot=True,
+    save_path="plots",
+    show_plot=False,
 ):
     if save_plot:
         os.makedirs(save_path, exist_ok=True)
@@ -148,15 +154,27 @@ def check_overfitting(model, x_train, y_train, x_test, y_test, model_name):
 
 
 # TODO: finish this method
-def metrics_report(x_train, y_train, model_name):
+def metrics_report(x_train, y_train, y_train_real):
     # Check for noisy points (labeled as -1)
     n_noise = list(y_train).count(-1)
     print(f"Number of noise points: {n_noise}")
 
-    # Calculate silhouette score (only if more than 1 cluster)
+    # Calculate the estimated number of clusters (excluding noise points)
+    n_clusters = len(set(y_train)) - (1 if -1 in y_train else 0)
+    print(f"Estimated number of clusters: {n_clusters}")
+
+    # Calculate Adjusted Rand Index (ARI) using true labels (X_train) and predicted labels (y_train)
+    ari_score = adjusted_rand_score(y_train_real, y_train)
+    print(f"Adjusted Rand Index: {ari_score:.2f}")
+
+    # Calculate Adjusted Mutual Information (AMI) using true labels (X_train) and predicted labels (y_train)
+    ami_score = adjusted_mutual_info_score(y_train_real, y_train)
+    print(f"Adjusted Mutual Information: {ami_score:.2f}")
+
+    # Calculate Silhouette Score (only if more than 1 cluster and not all are noise)
     if len(set(y_train) - {-1}) > 1:
         silhouette_avg = silhouette_score(x_train, y_train)
-        print(f"Silhouette Score for {model_name}: {silhouette_avg:.2f}")
+        print(f"Silhouette Score: {silhouette_avg:.2f}")
     else:
         silhouette_avg = None
         print("Silhouette Score cannot be calculated (only one cluster found).")
@@ -198,16 +216,16 @@ def grid_search_hyperparameters(models, param_grid, x_train, y_train):
 
 
 def plot_decision_boundary_helper(
-        x,
-        y,
-        model,
-        x_label,
-        y_label,
-        title,
-        filename="decision_boundary.png",
-        save_plot=True,
-        save_path="plots",
-        show_plot=False,
+    x,
+    y,
+    model,
+    x_label,
+    y_label,
+    title,
+    filename="decision_boundary.png",
+    save_plot=True,
+    save_path="plots",
+    show_plot=False,
 ):
     plt.figure(figsize=(8, 6))
     x0, x1 = x[:, 0], x[:, 1]
@@ -239,15 +257,15 @@ def plot_decision_boundary_helper(
 
 # TODO: implement
 def plot_boundaries(
-        dataset_id,
-        x,
-        y,
-        model,
-        model_name,
-        filename="decision_boundary.png",
-        save_plot=True,
-        save_path="plots",
-        show_plot=False,
+    dataset_id,
+    x,
+    y,
+    model,
+    model_name,
+    filename="decision_boundary.png",
+    save_plot=True,
+    save_path="plots",
+    show_plot=False,
 ):
     if dataset_id == DATASET_CIRCLES_ID:
         pass
@@ -258,7 +276,9 @@ def plot_boundaries(
         print(f"From {func_name}: can't plot, unsupported dataset or dimensions")
 
 
-def visualize_clusters(x_train, y_train_labels, model_name, save_path="plots", show_plot=False):
+def visualize_clusters(
+    x_train, y_train_labels, model_name, save_path="plots", show_plot=False
+):
     """Enhanced visualization for DBSCAN clustering."""
     plt.figure(figsize=(10, 8))
     unique_labels = set(y_train_labels)
@@ -269,29 +289,37 @@ def visualize_clusters(x_train, y_train_labels, model_name, save_path="plots", s
     for k, col in zip(unique_labels, colors):
         if k == -1:
             # Black color for noise points
-            col = 'k'
+            col = "k"
             label = "Noise"
             alpha = 0.3  # More transparency for noise
-            marker = 'x'  # Distinct marker for noise
+            marker = "x"  # Distinct marker for noise
         else:
             label = f"Cluster {k}"
             alpha = 0.8
-            marker = 'o'  # Circle marker for clusters
+            marker = "o"  # Circle marker for clusters
 
         # Mask for the current cluster points
-        class_member_mask = (y_train_labels == k)
+        class_member_mask = y_train_labels == k
         xy = x_train[class_member_mask]
 
         # Visualize each cluster
-        plt.scatter(xy[:, 0], xy[:, 1], c=[col], edgecolor='k',
-                    s=50, alpha=alpha, label=label, marker=marker)
+        plt.scatter(
+            xy[:, 0],
+            xy[:, 1],
+            c=[col],
+            edgecolor="k",
+            s=50,
+            alpha=alpha,
+            label=label,
+            marker=marker,
+        )
 
     # Set titles and labels
     plt.title(f"Enhanced DBSCAN Clustering for {model_name}")
     plt.xlabel("Feature 1")
     plt.ylabel("Feature 2")
     plt.legend(loc="best", markerscale=1.0)
-    plt.grid(True, color='grey', linestyle='--', linewidth=0.5)
+    plt.grid(True, color="grey", linestyle="--", linewidth=0.5)
 
     # Save or show the plot
     os.makedirs(save_path, exist_ok=True)
@@ -305,40 +333,46 @@ def visualize_clusters(x_train, y_train_labels, model_name, save_path="plots", s
 
 
 def evaluate_model(
-        model,
-        model_name,
-        used_dataset_id,
-        x_train_dataframe,
-        y_train_dataframe=None,  # not used in DBSCAN
-        x_test_dataframe=None,  # not used in DBSCAN
-        y_test_dataframe=None,  # not used in DBSCAN
-        save_plots_flg=False,
-        plots_path="plots",
-        show_plot_flg=False,
+    model,
+    model_name,
+    used_dataset_id,
+    x_train_dataframe,
+    y_train_dataframe,
+    x_test_dataframe,
+    y_test_dataframe,
+    save_plots_flg=False,
+    plots_path="plots",
+    show_plot_flg=False,
 ):
     start_time = time.time()  # Start timing
     model.fit(x_train_dataframe)
     end_time = time.time()  # End timing
 
     # Print clustering time
-    print(f"Clustering time for {model_name}: {
-          end_time - start_time:.2f} seconds")
+    print(
+        f"Clustering time: {
+            end_time - start_time:.2f} seconds"
+    )
 
     y_train_labels = model.labels_
 
-    metrics_report(x_train_dataframe, y_train_labels, model_name)
+    metrics_report(x_train_dataframe, y_train_labels, y_train_dataframe)
 
     if save_plots_flg:
-        visualize_clusters(x_train_dataframe, y_train_labels,
-                           model_name, save_path=plots_path, show_plot=show_plot_flg)
+        visualize_clusters(
+            x_train_dataframe,
+            y_train_labels,
+            model_name,
+            save_path=plots_path,
+            show_plot=show_plot_flg,
+        )
+
 
 # Update load_dataset function to support larger datasets
-
-
-def load_dataset(dataset_id: int, n_samples: int = 10000) -> pd.DataFrame | None:
+def load_large_dataset(dataset_id: int, n_samples: int = 10000) -> pd.DataFrame | None:
     if dataset_id == DATASET_CIRCLES_ID:
-        X, y = make_circles(n_samples, factor=.1, noise=.1)
-        dataset = pd.DataFrame(X, columns=["Feature_1", "Feature_2"])
+        x, y = make_circles(n_samples, factor=0.1, noise=0.1)
+        dataset = pd.DataFrame(x, columns=["Feature_1", "Feature_2"])
         dataset["Label"] = y.astype(int)
         return dataset
 
@@ -348,10 +382,14 @@ def load_dataset(dataset_id: int, n_samples: int = 10000) -> pd.DataFrame | None
         n_samples_3 = n_samples - n_samples_1 - n_samples_2
         centers = [[0.0, 0.0], [2.5, 2.5], [-2.5, -2.5]]
         clusters_std = [1.5, 0.5, 1.0]
-        X, y = make_blobs(n_samples=[n_samples_1, n_samples_2, n_samples_3],
-                          centers=centers, cluster_std=clusters_std,
-                          random_state=0, shuffle=False)
-        dataset = pd.DataFrame(X, columns=["Feature_1", "Feature_2"])
+        x, y = make_blobs(
+            n_samples=[n_samples_1, n_samples_2, n_samples_3],
+            centers=centers,
+            cluster_std=clusters_std,
+            random_state=0,
+            shuffle=False,
+        )
+        dataset = pd.DataFrame(x, columns=["Feature_1", "Feature_2"])
         dataset["Label"] = y.astype(int)
         return dataset
     else:
@@ -362,8 +400,8 @@ def load_dataset(dataset_id: int, n_samples: int = 10000) -> pd.DataFrame | None
 
 
 if __name__ == "__main__":
-    # dataset_id_for_use = DATASET_CIRCLES_ID
-    dataset_id_for_use = DATASET_BLOBS_ID
+    dataset_id_for_use = DATASET_CIRCLES_ID
+    # dataset_id_for_use = DATASET_BLOBS_ID
 
     log_to_file_flag = False
     log_path = "output_log.txt"
@@ -394,9 +432,7 @@ if __name__ == "__main__":
 
     # Load the dataset
     dataframe = load_dataset(dataset_id_for_use)
-    plot_dataset(
-        dataset_id_for_use, dataframe, save_path=plots_save_path
-    )
+    plot_dataset(dataset_id_for_use, dataframe, save_path=plots_save_path)
     # TODO: Чи є розбиття стабiльним пiсля змiни порядку об’єктiв у множинi об’єктiв?
     # Shuffle the dataframe and split it into training and test sets
     split_index = int(TRAIN_SPLIT_RATIO * len(dataframe))
@@ -410,14 +446,16 @@ if __name__ == "__main__":
     X_test_df, y_test_df = separate_dataset(test, "Label")
 
     model = DBSCAN()
+    model_name = f"DBSCAN_{model.metric}_{
+        model.eps}_{model.min_samples}_{model.p}"
 
-    print(f"Model: DBSCAN with {model.metric} metric")
+    print("\nModel:", model_name)
     print("Dataset:", get_dataset_name_by_id(dataset_id_for_use))
 
     # Evaluate the model
     evaluate_model(
         model,
-        f"DBSCAN with {model.metric} metric",
+        model_name,
         dataset_id_for_use,
         X_train_df,
         y_train_df,
@@ -447,12 +485,15 @@ if __name__ == "__main__":
             metric=metric,
             p=p
         )
+        model_name = f"DBSCAN_{metric}_{eps}_{min_samples}_{p}"
+
+        print("\nModel:", model_name)
+        print("Dataset:", get_dataset_name_by_id(dataset_id_for_use))
 
         # Evaluate the model using the evaluate_model function
         evaluate_model(
             new_model,
-            f"DBSCAN with {metric} metric, eps={eps}, min_samples={
-                min_samples}, p={p}",
+            model_name,
             dataset_id_for_use,
             X_train_df,
             y_train_df,
@@ -465,7 +506,7 @@ if __name__ == "__main__":
 
     # Define hyperparameter grids for each classifier
     # TODO: grid search on eps, min_samples and other params except metrics
-    '''
+    """
     param_grids = {
         "MLPClassifier_3": {
             "hidden_layer_sizes": [(3,), (5,), (10,)],
@@ -527,7 +568,7 @@ if __name__ == "__main__":
             plots_save_path,
             show_plot_flag,
         )
-    '''
+    """
     if log_to_file_flag:
         log_file.close()
         sys.stdout = original_stdout
